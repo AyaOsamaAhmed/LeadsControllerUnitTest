11547ea890333e8271bdacc281487c104abf897c
aya osama <ayaosamaahmed.1993@gmail.com>
2025-12-06 22:22:11 +0200

    Refactor LeadsController to use a Set for leads and add input validation for adding leads

--- app/src/main/java/com/aya/leadscontrollerunittest/LeadsController.kt
+++ app/src/main/java/com/aya/leadscontrollerunittest/LeadsController.kt
@@ -2,16 +2,30 @@ package com.aya.leadscontrollerunittest
 
 class LeadsController {
 
-    private var leadsList = mutableListOf<Lead>()
+
+    //testing on public function only
+
+    private var leadsList = mutableSetOf<Lead>()
 
     fun addLead(lead: Lead) {
+        if(isValidInput(lead.firstName) && isValidInput(lead.lastName) && isValidPhoneNumber(lead.phoneNumber))
         leadsList.add(lead)
     }
 
-    fun getLeads() : List<Lead> {
+    fun getLeads() : Set<Lead> {
         return leadsList
 
     }
 
+    private fun isValidInput(input : String): Boolean{
+        return input.length >= 3
+    }
+
+    private fun isValidPhoneNumber(input : String): Boolean{
+        if(input.all { char -> char.isLetter() })
+            throw IllegalArgumentException("Phone number can't contain letters")
+
+        return input.length == 11 && input.all { char -> char.isDigit() }
+    }
 
 }
--- app/src/test/java/com/aya/leadscontrollerunittest/LeadControllerTest.kt
+++ app/src/test/java/com/aya/leadscontrollerunittest/LeadControllerTest.kt
@@ -1,19 +1,121 @@
 package com.aya.leadscontrollerunittest
 
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
+import kotlin.jvm.Throws
+
+
+// Rule **
+// Rules allow very flexible addition or redefinition of the behavior of each test method in a test class.
+// Testers can reuse or extend one of the provided Rules below, or write their own.
+// https://github.com/junit-team/junit4/wiki/Rules
+
+/*suite **
+ collect some classes for testing together
+ https://github.com/junit-team/junit4/wiki/Aggregating-tests-in-suites
+ @RunWith(Suite.class)
+@Suite.SuiteClasses({
+  TestFeatureLogin.class,
+  TestFeatureLogout.class,
+  TestFeatureNavigate.class,
+})
+**/
 
 class LeadControllerTest {
 
+    private var controller : LeadsController? = null
+
+
+
+    @Before  // call before each test   VS.  @BeforeClass  // call only once before all tests
+    fun setup(){
+        controller = LeadsController()
+        print("setup ..... called")
+    }
     @Test
     fun `Given valid lead when call addLoad() then we should expect lead list has on item`(){
         val lead = Lead("Aya","Osama", "01001059357")
-        val controller = LeadsController()
-        controller.addLead(lead)
 
-        assertEquals(1, controller.getLeads().size)
+        controller?.addLead(lead)
+
+        assertEquals(1, controller?.getLeads()?.size)
+
+    }
+
+    @Test
+    fun `Given lead with two char in first name when call addLoad() then we should expect that lead is empty`(){
+        val lead = Lead("AA","Osama", "01001059357")
+        controller?.addLead(lead)
+
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
+
+    }
+
+    @Test
+    fun `Given lead with two char in last name when call addLoad() then we should expect that lead is empty`(){
+        val lead = Lead("Aya","Os", "01001059357")
+        controller?.addLead(lead)
+
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
+
+    }
+
+    @Test
+    fun `Given lead with not vaild phone number when call addLoad() then we should expect that lead is empty`(){
+        val lead = Lead("Aya","Osama", "0100105")
+        controller?.addLead(lead)
+
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
+
+    }
+
+    @Test
+    fun `Given lead with not vaild phone number with 12 when call addLoad() then we should expect that lead is empty`(){
+        val lead = Lead("Aya","Osama", "010010593577")
+        controller?.addLead(lead)
+
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
 
     }
 
 
+    @Test(expected = IllegalArgumentException::class)
+    fun `Given lead with   phone number with 11 chars when call addLoad() then we should expect that lead is empty`(){
+        val lead = Lead("Aya","Osama", "AAAAAAAAAAA")
+        controller?.addLead(lead)
+
+        assertTrue(controller?.getLeads()?.isEmpty() == true)
+
+    }
+
+
+    @Ignore("ignore this test")
+    @Test
+    fun `Given lead with  vaild phone number when call addLoad() then we should expect that lead is non empty`(){
+        val lead = Lead("Aya","Osama", "01001059357")
+        controller?.addLead(lead)
+
+        assertEquals(1,controller?.getLeads()?.size )
+
+    }
+
+    @Test
+    fun `Given lead with  duplicated load when call addLoad() then we should expect that lead is non empty`(){
+        val lead = Lead("Aya","Osama", "01001059357")
+        controller?.addLead(lead)
+        controller?.addLead(lead)
+
+        assertEquals(1,controller?.getLeads()?.size )
+
+    }
+
+    @After
+    fun tearDown() {
+        controller = null
+        print("tearDown ..... called")
+    }
 }
